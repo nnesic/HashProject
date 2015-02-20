4b670cf326300e807a2730fde24def601a8f9666
Nera <nera@NeraThesis.(none)>
2015-02-20 13:08:29 +0000

    Random positioning with tokens

--- hash.py
+++ hash.py
@@ -6,13 +6,17 @@ import sys
 
 class Node(object):
     
-    def __init__(self, position):
+    def __init__(self, position, parent=None):
         self.data = []
         self.position = position    #position on the ring
         self.writes_seen = 0
+        self.real_node = parent       #if this is just a token, this points to the real node
 
     def write(self):
-        self.writes_seen += 1
+        if (self.real_node != None):
+            self.real_node.write()
+        else:
+            self.writes_seen += 1
 
     def clean_writes(self):
         self.writes_seen = 0
@@ -28,111 +32,131 @@ class Node(object):
 class Ring(object):
 
     def __init__(self, S, E, N, W, I, Sm):
-        self.nodes = []
+        self.nodes = []         # real and tokens
+        self.real_nodes = []
         self.E = E
         self.I = I
         self.W = W
         self.N = N
         self.Sm = Sm
+        self.add_nodes = self.add_nodes_randomly_with_tokens   #method used to add nodes
 
-        self.add_nodes_randomly(S)
-
-        root = Tk()
-        self.ex = DrawIt(root, self.E, self)
-        #ex.draw_nodes(self.nodes)
-        root.geometry("1000x1000")
-        root.mainloop()  
+        self.add_nodes(S)
+        self.evaluate()
+        # print "real nodes"
+        # for node in self.real_nodes:
+        #     print "Position: %d writes: %d" %(node.position, node.writes_seen)
+        # print "all nodes"
+        # for node in self.nodes:
+        #     print "Position: %d writes: %d parent position: %d"  %(node.position, node.writes_seen, node.real_node.position if node.real_node != None else -1 )
+        self.add_servers_and_evaluate()
 
 
     def add_nodes_randomly(self, numNodes):
         for i in range (0, numNodes):
-            if (len(self.nodes) >= self.Sm):
+            if (self.count_real_nodes() >= self.Sm):
                 break
             next_server = random.randint(0, self.E-1)
             while (self.is_taken(next_server)):
                 next_server = random.randint(0, self.E-1)
             self.nodes += [Node(next_server)]
 
+        self.nodes.sort(key=lambda x: x.position, reverse=False)
+
+
+    def add_nodes_randomly_with_tokens(self, numNodes):
+        for i in range (0, numNodes):
+            if (self.count_real_nodes() >= self.Sm):
+                break
+            next_server = random.randint(0, self.E-1)
+            while (self.is_taken(next_server)):
+                next_server = random.randint(0, self.E-1)
+            real_node = Node(next_server)
+            self.nodes += [real_node]
+            self.real_nodes += [real_node]
+            # Now add the tokens
+            tokens = 5
+            for i in range (0, tokens):
+                next_server = random.randint(0, self.E-1)
+                while (self.is_taken(next_server)):
+                    next_server = random.randint(0, self.E-1)
+                self.nodes += [Node(next_server, real_node)]
+
+
+        self.nodes.sort(key=lambda x: x.position, reverse=False)
+        
 
     def is_taken(self, position):
         for node in self.nodes:
-            if node.get_position() == position:
+            if node.position == position:
                 return True
-        return False
-
-    def add_node(self, node):
-        self.nodes += [node]
-
-    def add_nodes_in_increment(self):
-        self.add_nodes_randomly(self.I)
-        self.ex.draw_nodes(self.nodes)
+        return False 
 
 
     def write_to_node(self, key_hash):
         min_distance = self.E + 1
-        for node in self.nodes:
-            for 
+        node = -1
+        #find the node at which to start writing
+        for i in range (0, len(self.nodes)):
+            if self.nodes[i].position > key_hash and (self.nodes[i].position - key_hash) < min_distance:
+                min_distance = self.nodes[i].position - key_hash
+                node = i
+        #write the replicas of the record
+        if self.nodes[node].real_node != None:
+            #if this is a token, we need to find the index of its parent
+            parent_position = self.nodes[node].real_node.position
+            for i in range (0, len(self.real_nodes)):
+                if self.real_nodes[i].position == parent_position:
+                    node = i
+                    break
+        for r in range (0, self.N):
+            self.real_nodes[(node + r) % len(self.real_nodes)].write()
+
+
+    def add_servers_and_evaluate(self):
+        while len(self.real_nodes) < self.Sm:
+            print "Adding servers"
+            self.add_nodes(self.I)
+            self.evaluate()
+
 
     def evaluate(self):
         for node in self.nodes:
             node.clean_writes()
         for i in range(0, self.W):
-            key_hash = math.randint(0, self.E-1)
+            key_hash = random.randint(0, self.E-1)
+            self.write_to_node(key_hash)
+        self.print_info()
 
 
+    def count_real_nodes(self):
+        return len(self.real_nodes)
 
 
+    def print_info(self):
+        #for node in self.nodes:
+            # print "Position: %d writes: %d" % (node.position, node.get_writes_seen())
+        #import pdb; pdb.set_trace()
+        dev = self.get_standard_deviation()
+        desired = self.W * self.N / len(self.real_nodes)
+        percentage_offset = dev * 100 / desired
+        print "Number of servers: %d Standard deviation: %d (%d%% off desired load)" % (self.count_real_nodes(), dev, percentage_offset)
 
 
-class DrawIt(Frame):
-  
-    def __init__(self, parent, E, ring):
+    def get_standard_deviation(self):
+        count = self.count_real_nodes()
+        #mean
+        mean = 0
+        for node in self.real_nodes:
+            mean += node.get_writes_seen()
+        mean /= count
+        #difference of squares
+        o = 0
+        for node in self.real_nodes:
 
-        Frame.__init__(self, parent)   
-        self.parent = parent 
-        self.E = E       
-        self.ring = ring
-        self.radius = 400
-        self.window = 1000
-        self.canvas = Canvas(self)
-        self.initUI()
-
-
-        
-    def initUI(self):
-      
-        self.parent.title("Colors")        
-        self.pack(fill=BOTH, expand=1)
-        #canvas = Canvas(self)
-        self.draw_circle(self.window/2, self.window/2, self.radius)
-        button1 = Button(self.canvas, text = "Add Nodes", command = self.ring.add_nodes_in_increment, anchor= W)
-        button1.configure(width = 10, activebackground = "#33B5E5",background = "#33B5E5", relief = FLAT)
-        button1_window = self.canvas.create_window(10, 10, anchor=NW, window=button1)
-        button1.pack(side=TOP)
-
-        # button2 = Button(self.canvas, text = "", command = self.ring.clear)
-        # button2.configure(width = 10, activebackground = "#33B5E5",background = "#33B5E5", relief = FLAT)
-        # button2_window = self.canvas.create_window(10, 10, anchor=NW, window=button2)
-        # button2.pack(side=TOP)
-
-        self.draw_nodes(self.ring.nodes)
-
-        self.canvas.pack(fill=BOTH, expand=1)
-
-
-    def draw_circle(self, x, y, r, **kwargs):
-        self.canvas.create_oval(x-r, y-r, x+r, y+r, **kwargs)
-
-    def draw_nodes(self, positions):
-        for node in positions:
-            self.draw_node(node)
-
-    def draw_node(self, node):
-        position = node.get_position()
-        y = math.sin(position * math.pi * 2 / self.E) * self.radius
-        x = math.cos(position * math.pi * 2 / self.E) * self.radius
-        rad = 10
-        self.draw_circle(self.window/2 + x, self.window/2 + y, rad, fill="#05f")
+            o += (node.get_writes_seen() - mean)**2
+        o /= count
+        return o**0.5
 
 def main():
 
@@ -151,7 +175,8 @@ def main():
     args=parser.parse_args()
 
     ring = Ring(args.S, args.E, args.N, args.W, args.I, args.Sm)
-
+    #ring.evaluate()
+    #ring.print_info()
 
 
 
